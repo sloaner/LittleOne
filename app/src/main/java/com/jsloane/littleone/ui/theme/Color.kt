edb8e2b8bec09bf75acd629e46e26efc0aed3e6f
Jeremy Sloane <jeremy.sloane@exeloncorp.com>
2021-10-21 00:17:16 -0400

    New Theme

--- app/src/main/java/com/jsloane/littleone/ui/theme/Color.kt
+++ app/src/main/java/com/jsloane/littleone/ui/theme/Color.kt
@@ -10,3 +10,10 @@ val GreenDark = Color(0xFF2D7B38)
 
 val DarkText = Color(0xFF121212)
 val LightText = Color(0xFFFFFFFF)
+
+val PurpleDark = Color(0xFF413D8A)
+val Blush = Color(0xFFEFDDE0)
+val BlushDark = Color(0xFFDFC4C8)
+val PurpleGray = Color(0xFFC3BBC6)
+val LightGray = Color(0xFFF1F1F1)
+val OffWhite = Color(0xFFFEF8F9)
--- app/src/main/java/com/jsloane/littleone/ui/theme/Theme.kt
+++ app/src/main/java/com/jsloane/littleone/ui/theme/Theme.kt
@@ -1,40 +1,70 @@
 package com.jsloane.littleone.ui.theme
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.Card
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.FabPosition
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.material.darkColors
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.jsloane.littleone.R
 
 private val LightColorPalette = lightColors(
-    primary = PinkMedium,
-    primaryVariant = PinkDark,
-    secondary = GreenMedium,
-    secondaryVariant = GreenDark,
-    onPrimary = Color.Black
+    primary = PurpleDark,
+    primaryVariant = PurpleDark,
+    onPrimary = OffWhite,
+    secondary = PurpleGray,
+    secondaryVariant = PurpleGray,
+    onSecondary = DarkText,
+    background = Blush,
+    onBackground = PurpleDark,
+    surface = OffWhite,
+    onSurface = PurpleDark
 )
 
 private val DarkColorPalette = darkColors(
-    primary = PinkDark,
-    primaryVariant = PinkLight,
-    onPrimary = Color.White,
-    secondary = GreenDark,
-    secondaryVariant = GreenMedium,
-    onSecondary = Color.White
+    primary = BlushDark,
+    primaryVariant = Blush,
+    onPrimary = DarkText,
+    secondary = PurpleGray,
+    secondaryVariant = PurpleGray,
+    onSecondary = DarkText
 )
 
 @Composable
@@ -155,6 +185,128 @@ private fun drawSwatch(name: String, color: Color, onColor: Color? = null) {
                         style = MaterialTheme.typography.body1,
                         color = readableTextColor(onColor)
                     )
+                }
+            }
+        }
+    }
+}
+
+@Preview("Gallery - Day")
+@Preview("Gallery - Night", uiMode = UI_MODE_NIGHT_YES)
+@Composable
+private fun Gallery() {
+    LittleOneTheme {
+        Scaffold(
+            bottomBar = {
+                BottomAppBar(cutoutShape = CircleShape) {
+                    IconButton(onClick = {}) {
+                        Icon(Icons.Filled.Menu, "")
+                    }
+                    Spacer(Modifier.weight(1f, true))
+                    IconButton(onClick = {}) {
+                        Icon(Icons.Filled.Share, "")
+                    }
+                    IconButton(onClick = {}) {
+                        Icon(Icons.Filled.Search, "")
+                    }
+                }
+            },
+            floatingActionButton = {
+                FloatingActionButton(
+                    shape = CircleShape,
+                    onClick = {}
+                ) {
+                    Icon(Icons.Filled.Add, "")
+                }
+            },
+            floatingActionButtonPosition = FabPosition.Center,
+            isFloatingActionButtonDocked = true
+        ) {
+            Column(
+                modifier = Modifier.padding(8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                repeat(3) {
+                    Card(elevation = 2.dp) {
+                        Column {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Box(
+                                    Modifier
+                                        .padding(16.dp)
+                                        .background(Color(0xFFE6E6E6), CircleShape)
+                                        .size(40.dp)
+                                        .padding(8.dp)
+                                ) {
+                                    Image(painterResource(id = R.drawable.ic_preview), "")
+                                }
+                                Column(
+                                    modifier = Modifier
+                                        .weight(1f)
+                                        .padding(bottom = 2.dp)
+                                ) {
+                                    Text(
+                                        text = "Headline 6",
+                                        style = MaterialTheme.typography.h6
+                                    )
+                                    Text(
+                                        modifier = Modifier.alpha(ContentAlpha.medium),
+                                        text = "Body 2",
+                                        style = MaterialTheme.typography.body2
+                                    )
+                                }
+                                Icon(
+                                    Icons.Default.Share, "",
+                                    modifier = Modifier.padding(24.dp)
+                                )
+                            }
+                            Image(
+                                modifier = Modifier
+                                    .padding(4.dp)
+                                    .background(Color(0xFFE6E6E6))
+                                    .padding(32.dp)
+                                    .fillMaxWidth()
+                                    .aspectRatio(2f),
+                                painter = painterResource(id = R.drawable.ic_preview),
+                                contentDescription = ""
+                            )
+                            Row(
+                                modifier = Modifier.padding(horizontal = 4.dp),
+                                horizontalArrangement = Arrangement.spacedBy(4.dp)
+                            ) {
+                                repeat(3) {
+                                    Image(
+                                        modifier = Modifier
+                                            .background(Color(0xFFE6E6E6))
+                                            .padding(32.dp)
+                                            .aspectRatio(1f)
+                                            .weight(1f),
+                                        painter = painterResource(id = R.drawable.ic_preview),
+                                        contentDescription = ""
+                                    )
+                                }
+                            }
+                            Text(
+                                modifier = Modifier
+                                    .padding(horizontal = 8.dp, vertical = 12.dp)
+                                    .alpha(ContentAlpha.medium),
+                                text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                                    "sed do eiusmod tempor",
+                                style = MaterialTheme.typography.body2,
+                            )
+                            Row {
+                                repeat(2) {
+                                    TextButton(
+                                        modifier = Modifier.padding(8.dp),
+                                        onClick = {}
+                                    ) {
+                                        Text("BUTTON")
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
