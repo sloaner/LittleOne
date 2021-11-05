4cfd8a4f22493cf7fed4bbfcfd0b2d99ca70e7d8
Jeremy Sloane <jeremy.sloane@exeloncorp.com>
2021-11-04 22:57:58 -0400

    Added gradle dependency version checker

--- buildSrc/src/main/kotlin/com/jsloane/littleone/buildsrc/Dependencies.kt
+++ buildSrc/src/main/kotlin/com/jsloane/littleone/buildsrc/Dependencies.kt
@@ -11,7 +11,7 @@ object Libs {
     }
 
     object Accompanist {
-        const val version = "0.19.0"
+        const val version = "0.20.1"
         const val insets = "com.google.accompanist:accompanist-insets:$version"
         const val systemuicontroller = "com.google.accompanist:accompanist-systemuicontroller:$version"
         const val flowlayouts = "com.google.accompanist:accompanist-flowlayout:$version"
@@ -35,7 +35,7 @@ object Libs {
     }
 
     object Material {
-        const val material = "com.google.android.material:material:1.3.0"
+        const val material = "com.google.android.material:material:1.4.0"
     }
 
     object PlayServices {
@@ -44,14 +44,14 @@ object Libs {
 
     object Firebase {
         const val gradlePlugin = "com.google.gms:google-services:4.3.10"
-        const val bom = "com.google.firebase:firebase-bom:28.4.2"
+        const val bom = "com.google.firebase:firebase-bom:29.0.0"
         const val analytics = "com.google.firebase:firebase-analytics-ktx"
         const val auth = "com.google.firebase:firebase-auth-ktx"
         const val firestore = "com.google.firebase:firebase-firestore-ktx"
     }
 
     object Hilt {
-        private const val version = "2.39.1"
+        private const val version = "2.40"
         const val gradlePlugin = "com.google.dagger:hilt-android-gradle-plugin:$version"
         const val compiler = "com.google.dagger:hilt-android-compiler:$version"
         const val hilt = "com.google.dagger:hilt-android:$version"
@@ -60,34 +60,36 @@ object Libs {
     }
 
     object AndroidX {
-        const val coreKtx = "androidx.core:core-ktx:1.6.0"
+        const val coreKtx = "androidx.core:core-ktx:1.7.0"
 
         object Compose {
-            const val snapshot = ""
-            const val version = "1.0.4"
+            const val version = "1.0.5"
 
-            const val foundation = "androidx.compose.foundation:foundation:${version}"
-            const val layout = "androidx.compose.foundation:foundation-layout:${version}"
-            const val ui = "androidx.compose.ui:ui:${version}"
-            const val uiUtil = "androidx.compose.ui:ui-util:${version}"
-            const val runtime = "androidx.compose.runtime:runtime:${version}"
-            const val material = "androidx.compose.material:material:${version}"
-            const val animation = "androidx.compose.animation:animation:${version}"
-            const val tooling = "androidx.compose.ui:ui-tooling:${version}"
+            const val foundation = "androidx.compose.foundation:foundation:$version"
+            const val layout = "androidx.compose.foundation:foundation-layout:$version"
+            const val ui = "androidx.compose.ui:ui:$version"
+            const val uiUtil = "androidx.compose.ui:ui-util:$version"
+            const val runtime = "androidx.compose.runtime:runtime:$version"
+            const val material = "androidx.compose.material:material:$version"
+            const val animation = "androidx.compose.animation:animation:$version"
+            const val tooling = "androidx.compose.ui:ui-tooling:$version"
             const val iconsExtended = "androidx.compose.material:material-icons-extended:$version"
             const val uiTest = "androidx.compose.ui:ui-test-junit4:$version"
         }
 
-        object Activity {
-            const val activityCompose = "androidx.activity:activity-compose:1.3.1"
+        object UI {
+            const val activityCompose = "androidx.activity:activity-compose:1.4.0"
+            const val appcompat = "androidx.appcompat:appcompat:1.3.1"
+            const val constraintLayoutCompose =
+                "androidx.constraintlayout:constraintlayout-compose:1.0.0-rc01"
         }
 
-        object AppCompat {
-            const val appcompat = "androidx.appcompat:appcompat:1.3.1"
+        object Data {
+            const val datastore = "androidx.datastore:datastore-preferences:1.0.0"
         }
 
         object Lifecycle {
-            private const val version = "2.4.0-rc01"
+            private const val version = "2.4.0"
             const val viewModelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:$version"
             const val runtime = "androidx.lifecycle:lifecycle-runtime-ktx:$version"
         }
@@ -96,30 +98,25 @@ object Libs {
             const val navigationCompose = "androidx.navigation:navigation-compose:2.4.0-alpha10"
         }
 
-        object ConstraintLayout {
-            const val constraintLayoutCompose =
-                "androidx.constraintlayout:constraintlayout-compose:1.0.0-rc01"
-        }
-
         object Test {
             private const val version = "1.4.0"
             const val core = "androidx.test:core:$version"
             const val runner = "androidx.test:runner:$version"
             const val rules = "androidx.test:rules:$version"
             object Ext {
-                private const val version = "1.1.2"
+                private const val version = "1.1.3"
                 const val junit = "androidx.test.ext:junit-ktx:$version"
             }
-            const val espressoCore = "androidx.test.espresso:espresso-core:3.2.0"
+            const val espressoCore = "androidx.test.espresso:espresso-core:3.4.0"
         }
     }
 
     object JUnit {
-        private const val version = "4.13"
+        private const val version = "4.13.2"
         const val junit = "junit:junit:$version"
     }
 
     object Coil {
-        const val coilCompose = "io.coil-kt:coil-compose:1.3.2"
+        const val coilCompose = "io.coil-kt:coil-compose:1.4.0"
     }
 }
